b035f068fa951575886d77fb56ded3ce34aa8950
KevinBello11 <kebelloc@itc.edu.co>
2023-08-29 19:52:27 -0500

    Uploading KevinFront

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { PagosComponent } from './components/pagos/pagos.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    PagosComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
