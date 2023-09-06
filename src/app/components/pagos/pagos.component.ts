5921d233e8dc639a5a4592cf5118d8ab884cc1f7
KevinBello11 <kebelloc@itc.edu.co>
2023-09-05 19:27:50 -0500

    Uplading modules Kevin aboaut api services

--- src/app/components/pagos/pagos.component.ts
+++ src/app/components/pagos/pagos.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-pagos',
   templateUrl: './pagos.component.html',
   styleUrls: ['./pagos.component.css']
 })
-export class PagosComponent {
+export class PagosComponent implements OnInit {
 
   pagos = "Estos son los pagos";
 
+  constructor(public apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.Get("Arrendatarios");
+  }
 }
+
+
--- src/app/components/proyectos/proyectos.component.ts
+++ src/app/components/proyectos/proyectos.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements OnInit {
 
   proyectos = "Estos son los proyectos";
 
+  constructor(public apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.Get("Proyectos");
+  }
 }
+
+
