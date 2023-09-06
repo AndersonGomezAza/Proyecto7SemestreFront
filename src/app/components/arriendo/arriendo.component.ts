a44db67bddf65c8e16ef4b478a6db367c65519bb
Edrinconv <edrinconv@itc.edu.co>
2023-09-05 20:00:33 -0500

    Uploading EdwardServices

--- src/app/components/arriendo/arriendo.component.ts
+++ src/app/components/arriendo/arriendo.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-arriendo',
   templateUrl: './arriendo.component.html',
   styleUrls: ['./arriendo.component.css']
 })
-export class ArriendoComponent {
+export class ArriendoComponent implements OnInit{
 tituloArriendo="Arriendo"
+
+constructor(public apiService: ApiService) {}
+
+ngOnInit(): void {
+  this.apiService.Get("ContratoArriendoes");
+}
 }
--- src/app/components/inmueble/inmueble.component.ts
+++ src/app/components/inmueble/inmueble.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-inmueble',
   templateUrl: './inmueble.component.html',
   styleUrls: ['./inmueble.component.css']
 })
-export class InmuebleComponent {
+export class InmuebleComponent implements OnInit {
 tituloInmueble = "INMUEBLE"
+
+constructor(public apiService: ApiService) {}
+
+ngOnInit(): void {
+  this.apiService.Get("Inmuebles");
+}
 }
